28bd7456e26c49dfa9c70ba799421eca4129e993
Matthew Lin <Matthew Lin>
2017-03-13 11:50:11 -0400

    More practice with arrow functions

--- examplearrow.js
+++ examplearrow.js
@@ -1,7 +1,14 @@
 var names = ['Mike', 'Matt', 'Megan'];
 
-names.forEach(function(name){
-	console.log('forEach', name);
-});
+// names.forEach(function(name){
+// 	console.log('forEach', name);
+// });
 
-names.forEach((name) => console.log(name));
+// names.forEach((name) => console.log(name));
+
+var person = {
+	name: 'Matthew',
+	greet: function(){
+
+	}
+};
