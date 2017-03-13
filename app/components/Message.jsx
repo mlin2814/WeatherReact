cb68a8824190411d2528b4a12736c7117bfebbbd
Matthew Lin <Matthew Lin>
2017-03-13 12:37:19 -0400

    Refactored components to use arrow functions

--- app/components/Message.jsx
+++ app/components/Message.jsx
@@ -1,13 +1,9 @@
 var React = require('react');
 
-var Message = React.createClass({	
-	render: function () {
-    var {temp, location} = this.props;
-
-    return (
-      <h3>It's {temp} in {location}.</h3>
-    )
-  }
-});
+var Message = ({temp, location}) => {
+  return (
+    <h3>It's {temp} in {location}.</h3>
+  )
+};
 
 module.exports = Message;
--- examplearrow.js
+++ examplearrow.js
@@ -1,14 +1,32 @@
-var names = ['Mike', 'Matt', 'Megan'];
+// var names = ['Mike', 'Matt', 'Megan'];
 
-// names.forEach(function(name){
-// 	console.log('forEach', name);
+// names.forEach((name) => {
+// 	console.log('arrowFunc', name);
 // });
 
 // names.forEach((name) => console.log(name));
 
-var person = {
-	name: 'Matthew',
-	greet: function(){
+// var person = {
+// 	name: 'Matthew',
+// 	greet: function(){
+// 		names.forEach((name) => {
+// 			console.log(this.name + ' says hi to ' + name);
+// 		});
+// 	}
+// };
 
-	}
-};
+// person.greet();
+
+function add(a, b){
+	return a + b;
+}
+
+var addStatement = (a, b) => {
+	return a + b;	
+}
+console.log(addStatement(1, 3));
+
+
+var addExpression = (a, b) => a + b;
+console.log(addExpression(3, 2));
+
