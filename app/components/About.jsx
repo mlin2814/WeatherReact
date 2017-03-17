7e629e406d27e12ed96952d6e3e93133bc419d3f
Matthew Lin <Matthew Lin>
2017-03-17 12:08:38 -0400

    Updated about page with relevant content and links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,16 +1,21 @@
 var React = require('react');
-
-// var About = React.createClass({
-// 	render: function() {
-// 		return(
-// 			<h3>About Component</h3>	
-// 		);
-// 	}
-// });
+var {Link} = require('react-router');
 
 var About = (props) => {
 	return(
-		<h3>About Component</h3>	
+		<div>
+			<h1 className="text-center">About</h1>
+			<p>This application is a Weather application structured through React, Javascript ES6, and Foundation for styling. Type in any city, and the temperature will be returned to you.</p>
+			<p>As for me, I'm a full stack web developer looking to break into the development world and learn more about what it means to be a developer. This is just one of the many projects I've worked on and built, so take some time to look at the links below for more examples of my work. Enjoy!</p>
+			<ol>
+				<li>
+					<a href="https://sheltered-taiga-20526.herokuapp.com/">My Portfolio</a>
+				</li>
+				<li>
+					<a href="https://github.com/mlin2814">Github</a>
+				</li>
+			</ol>
+		</div>	
 	);
 };
 
