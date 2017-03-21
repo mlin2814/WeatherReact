eb118f9aada8b3824d5fba285a0ad8fdd699fdcd
Matthew Lin <Matthew Lin>
2017-03-21 11:47:37 -0400

    Adjusted routes for search bar and remove placeholders

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,13 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
 	onSearch:function(e){
 		e.preventDefault();
-		alert('Not ready!');
+		var location = this.refs.searchTerm.value;
+		var encodedLocation = encodeURIComponent(location);
+		if (location.length > 0) {
+			this.refs.searchTerm.value = '';
+			window.location.hash = '#/?location=' + encodedLocation;
+		}
+
 	},
 	render: function () {
 		return(
@@ -27,7 +33,7 @@ var Nav = React.createClass({
 					<form onSubmit={this.onSearch}>
 						<ul className="menu">
 					      <li>
-					      	<input type="search" placeholder="Search temperature by city"/>
+					      	<input type="search" placeholder="Search temperature by city" ref="searchTerm"/>
 					      </li>
 					      <li>
 					      	<input type="submit" className="button" value="Reveal the Weather!"/>
