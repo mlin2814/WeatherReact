bc3a6ae3e78757d6f2c618691f1238faa4517400
Matthew Lin <Matthew Lin>
2017-03-21 12:02:28 -0400

    Adjusted variables for heroku deployment

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,13 +4,14 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
 	onSearch:function(e){
 		e.preventDefault();
-		var location = this.refs.searchTerm.value;
+		
+		var location = this.refs.search.value;
 		var encodedLocation = encodeURIComponent(location);
+		
 		if (location.length > 0) {
-			this.refs.searchTerm.value = '';
+			this.refs.search.value = '';
 			window.location.hash = '#/?location=' + encodedLocation;
 		}
-
 	},
 	render: function () {
 		return(
@@ -33,7 +34,7 @@ var Nav = React.createClass({
 					<form onSubmit={this.onSearch}>
 						<ul className="menu">
 					      <li>
-					      	<input type="search" placeholder="Search temperature by city" ref="searchTerm"/>
+					      	<input type="search" placeholder="Search temperature by city" ref="search"/>
 					      </li>
 					      <li>
 					      	<input type="submit" className="button" value="Reveal the Weather!"/>
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -13,7 +13,6 @@ var Weather = React.createClass({
 	handleSearch: function(location){
 		var that = this;
 
-
 		this.setState({
 			isLoading: true,
 			errorMessage: undefined,
